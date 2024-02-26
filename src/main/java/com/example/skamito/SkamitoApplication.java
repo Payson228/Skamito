97705b50487818ee48d10299535f785a09ddb272
Payson228 <46265144+Payson228@users.noreply.github.com>
2024-02-27 01:20:10 +0600

    main

---
+++ src/main/java/com/example/skamito/SkamitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.skamito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkamitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkamitoApplication.class, args);
+	}
+
+}
