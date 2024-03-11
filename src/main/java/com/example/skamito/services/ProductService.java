89ab99ce653cd50eb8a84444132a033802fcc178
Payson228 <46265144+Payson228@users.noreply.github.com>
2024-03-12 04:27:51 +0600

    Add files via upload

--- src/main/java/com/example/skamito/services/ProductService.java
+++ src/main/java/com/example/skamito/services/ProductService.java
@@ -1,36 +1,54 @@
 package com.example.skamito.services;
 
 import com.example.skamito.models.Product;
+import com.example.skamito.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
-    private List<Product> products = new ArrayList<>();
-    private long ID = 0;
+    private final ProductRepository productRepository;
 
-    {
-        products.add(new Product(++ID, "PlayStation 5", "Simple description", 67000, "Krasnoyarsk", "tomas"));
-        products.add(new Product(++ID, "Iphone 8", "Simple description", 24000, "Moscow", "artyom"));
+    public List<Product> listProducts(String title) {
+        if(title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
 
-    public List<Product> listProducts() { return products; }
-
     public void saveProduct(Product product) {
-        product.setId(++ID);
-        products.add(product);
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)) return product;
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public void updateProduct(Long id, Product updatedProduct) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        if (productOptional.isPresent()) {
+            Product product = productOptional.get();
+            // Обновляем информацию о продукте
+            product.setTitle(updatedProduct.getTitle());
+            product.setDescription(updatedProduct.getDescription());
+            product.setPrice(updatedProduct.getPrice());
+            product.setCity(updatedProduct.getCity());
+            product.setAuthor(updatedProduct.getAuthor());
+            // Сохраняем обновленный продукт в базе данных
+            productRepository.save(product);
+        } else {
+            // Если продукт с указанным id не найден
+            throw new IllegalArgumentException("Product with id " + id + " not found");
         }
-        return null;
     }
 }
